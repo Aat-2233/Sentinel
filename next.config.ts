fb0d28ada8aada58433d0fd9609355721ec53fc4
Aat-2233 <aatreyeenag@gmail.com>
2026-03-29 20:00:02 +0530

    Simplify next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-  },
+  // Add config options here if needed in the future
 };
 
 export default nextConfig;
